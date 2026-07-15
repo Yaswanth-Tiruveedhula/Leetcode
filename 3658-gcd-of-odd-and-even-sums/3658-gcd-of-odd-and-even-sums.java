2f6cc9dbe5dc79e36da56fafaa2c57b5569859f8
Yaswanth-Tiruveedhula <yaswanth01sunny@gmail.com>
2026-07-15 11:17:35 +0530

    Time: 1 ms (83.11%), Space: 42.6 MB (39.85%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,19 +1,19 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        return oddsum(n);
+        return comf(n*n,n*(n+1));
     }
-    public int oddsum(int n){
-        int sum=0,i=1,eve=0,j=2;
-        while(n-- >0){
-            sum+=i;
-            eve+=j;
-            i+=2;
-            j+=2;
-        }
-        System.out.println(sum+" "+eve);
-        int gcd=comf(sum,eve);
-        return gcd;
-    }
+    // public int oddsum(int n){
+    //     int sum=0,i=1,eve=0,j=2;
+    //     while(n-- >0){
+    //         sum+=i;
+    //         eve+=j;
+    //         i+=2;
+    //         j+=2;
+    //     }
+    //     System.out.println(sum+" "+eve);
+    //     int gcd=comf(sum,eve);
+    //     return gcd;
+    // }
     public int comf(int a,int b){
         if(b==0)    return a;
         else return comf(b,a%b);
