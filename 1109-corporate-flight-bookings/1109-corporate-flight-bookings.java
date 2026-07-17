9a31314d18a8390a6abff5dcc3a04bd8866099e1
Yaswanth-Tiruveedhula <yaswanth01sunny@gmail.com>
2026-07-17 10:25:01 +0530

    Time: 3 ms (98.48%), Space: 84.4 MB (90.47%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
         int[] res=new int[n];
-        int ind=0;
+         int[] pre=new int[n];
         for(int[] i:bookings){
             int left=i[0]-1;
             int right=i[1]-1;
-            while(left<=right){
-                if(left==right){
-                    res[left++]+=i[2];
-                }else{
-                res[left++]+=i[2];
-                res[right--]+=i[2];
-            }
-            }
+            pre[left]+=i[2];
+            if(right<n-1) pre[right+1]-=i[2];
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=pre[i];
+            res[i]=sum;
         }
         return res;
     }
